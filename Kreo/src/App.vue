bf9a15a09b30f67e0395925eee50d689094d6b10
GaspareNavarra <gasparenavarra2@gmail.com>
2024-03-09 18:02:58 +0100

    First commit (Main page without data)

---
+++ Kreo/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="MainBody">
+    <div id="navbar" class="navbar">
+      <img src="/Img/Logo.png" class="logo_size">
+    </div>
+    
+    <DataSheet></DataSheet>
+  </div>
+</template>
+<script>
+import DataSheet from './components/DataSheet.vue';
+export default {
+  components: { DataSheet },
+}
+</script>
---
+++ Kreo/src/components/DataSheet.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="scheda-tecnica" class="datasheet_size">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
