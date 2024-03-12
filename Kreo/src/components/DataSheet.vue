ee777addc053ffb9b39b493ec29407e980d7a2c7
Gaspare Navarra <gaspare.navarra@labitech.com>
2024-03-12 17:57:37 +0100

    Added input

--- Kreo/src/components/DataSheet.vue
+++ Kreo/src/components/DataSheet.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="scheda-tecnica" class="datasheet_size row">
-        <!-- <div class="inputBox">
+        <div class="inputBox col-sm-2">
             <input type="text" required="required">
             <span>Nome</span>
-        </div> -->
+        </div>
     </div>
 </template>
 
