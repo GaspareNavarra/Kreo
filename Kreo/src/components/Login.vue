622a3fbea11c879b58bebda90b6f97051624a041
Gaspare Navarra <gaspare.navarra@labitech.com>
2024-03-13 15:38:49 +0100

    Login interface

--- Kreo/src/components/Login.vue
+++ Kreo/src/components/Login.vue
@@ -1,6 +1,27 @@
 <template>
     <div id="LoginPage">
-
+      <div class="container-login-input">
+        <div class="header">
+          <div class="title">KREO</div>
+          <div class="subtitle">Hair and Makeup Artist</div>
+        </div>
+        <div class="container-input">
+          <div class="inputBox col-sm-9">
+            <input type="text" required="required">
+            <span>Username</span>
+          </div>
+          <div class="inputBox col-sm-9">
+            <input type="text" required="required">
+            <span>Password</span>
+          </div>
+          <div id="actionButton" class="row no-margin">
+            <div id="checkBox" class="col-sm-9 checkbox-container">
+              <input type="checkbox"><div class="remember-label">Ricordami</div>
+            </div>
+            <button class="access-button no-padding col-sm-9">Accedi</button>
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
@@ -8,6 +29,7 @@
 export default {
   inject: ['classSelector'],
   mounted() {
+
     this.classSelector();
   }
 }
