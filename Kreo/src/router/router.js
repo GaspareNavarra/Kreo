2c0e167f32768e601d0a47e32488cfb5daae4663
Gaspare Navarra <gaspare.navarra@labitech.com>
2024-03-29 16:21:19 +0100

    Changed router webHistory

--- Kreo/src/router/router.js
+++ Kreo/src/router/router.js
@@ -7,7 +7,7 @@ import DataSheet from '@/components/DataSheet.vue';
 import NotFound from '@/components/NotFound.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(process.env.NODE_ENV),
   routes: [
     {path: '/', redirect: '/Login'},
     {path: '/Login', component: Login},
