2441a3c85ba33b6b095bbff4ac42736b005464ee
Gaspare Navarra <gaspare.navarra@labitech.com>
2024-07-16 16:38:06 +0200

    Fix main

--- Kreo/src/main.js
+++ Kreo/src/main.js
@@ -5,7 +5,9 @@ import router from './router/router';
 import './assets/css/main.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-window.BASE_URL_API = 'https://x8ki-letl-twmt.n7.xano.io/api:Fh-KZon-';
+window.BASE_URL_API_XANO = 'https://x8ki-letl-twmt.n7.xano.io/api:Fh-KZon-';
+//window.BASE_URL_API_CUSTOM = 'http://localhost:3000';
+window.BASE_URL_API_CUSTOM = 'https://kreo-be.vercel.app';
 
 const app = createApp(App);
 
